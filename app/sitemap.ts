a0a17235f2bba33bcfce8403524958cdf9cb01ad
Saksham Prajapati <sakshamprajapati@Sakshams-Macbook-Air.local>
2026-09-01 03:22:10 +0530

    fix: update sitemap domain to www.ddjc.org.in

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://ddjcprayas.org";
+  const baseUrl = "https://www.ddjc.org.in";
 
   return [
     {
